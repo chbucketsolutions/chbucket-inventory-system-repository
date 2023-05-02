73bb96bfccba9ba421f2c9d2c5748e96fdfe9796
Babayaga-cell <almirol.lexter@gmail.com>
2023-05-03 00:59:12 +0800

    [IS-31] Pushing unversioned files.

---
+++ src/main/java/chbucket/inventory/systemrepositoryservice/SystemRepositoryServiceApplication.java
@@ -0,0 +1,15 @@
+package chbucket.inventory.systemrepositoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SystemRepositoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemRepositoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chbucket/inventory/systemrepositoryservice/SystemRepositoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package chbucket.inventory.systemrepositoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemRepositoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
